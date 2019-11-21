5dd41ac5c44cd9fdc9e212a5833bfffe1211d8ed
manspider-peterporker <raphaelvieiraaas@gmail.com>
2019-11-21 09:44:00 -0300

    Q3 corrigida

--- Raphael_vieira_andré_da_silva_P2/Q3.py
+++ Raphael_vieira_andré_da_silva_P2/Q3.py
@@ -1,27 +1,52 @@
-def isCollisionDetected(coordA,coordB):
-    if(coordA('bottomleft')[0]<coordB['bottomleft'[0]]):
+def isCollisionDetected(rec1, rec2):
+    rec1_xi = rec1["bottomLeft"][0]
+    rec1_xf = rec1["topRight"][0]
+    rec1_yi = rec1["bottomLeft"][1]
+    rec1_yf = rec1["topRight"][1]
 
-        retanguloesquerdo = dict(coordA)
-        retangulodireito = dict(coordB)
+    rec2_xi = rec2["bottomLeft"][0]
+    rec2_xf = rec2["topRight"][0]
+    rec2_yi = rec2["bottomLeft"][1]
+    rec2_yf = rec2["topRight"][1]
 
-    else:
-        retanguloesquerdo = coordB
-        retangulodireito = coordA
-    x = retangulodireito['bottomlef']
-def vertice():
-    BLx = float(input('Entre com BLx'))
-    BLy = float(input('Entre com BLy'))
-    TRx = float(input('Entre com TRx'))
-    TRy = float(input('Entre com TRy'))
-    retangulo = {'bottomLeft':[BLx,BLy],'TopRight:[TRx,TRy]'}
-    return retangulo
+    if (rec1_xi < rec2_xf and rec1_xf > rec2_xi and rec1_yi < rec2_yf and rec1_yf > rec2_yi):
+        return True
+    return False
 
 def main():
-    retanguloA = vertice()
-    retanguloB = vertice()
+    rectangle = {"bottomLeft":"",
+             "topRight":""}
 
-        if isCollisionDetected(retanguloaA,retanguloB)==True:
-            print ('Colisão detectada')
+    BLx = float(input('Entre com o BLx para o primeiro retangulo'))
+    BLy = float(input('Entre com o BLy para o primeiro retangulo'))
+    TRx = float(input('Entre com o TRx para  o primeiro retangulo'))
+    TRy = float(input('Entre com o TRy para o primeiro retamgulo'))
 
-        else:
-            print('Colisão não detectada')
+    BL1 = (BLx, BLy)
+    TR1 = (TRx, TRy)
+    
+    rec1 = rectangle.copy()
+    rec1["bottomLeft"] = BL1
+    rec1["topRight"] = TR1
+    
+    BLx = float(input('Entre com o BLx para o segundo retangulo'))
+    BLy = float(input('Entre com o BLy para o segundo retangulo'))
+    TRx = float(input('Entre com o TRx para o segundo retangulo'))
+    TRy = float(input('Entre com o TRy para o segundo retangulo '))
+
+    BL2 = (BLx, BLy)
+    TR2 = (TRx, TRy)
+
+    rec2 = rectangle.copy()
+    rec2["bottomLeft"] = BL2
+    rec2["topRight"] = TR2
+
+    colidiu = isCollisionDetected(rec1, rec2)
+
+    if colidiu == True:
+        print("Colisao detectada")
+
+    else:
+        print("Colisao nao detectada")
+
+main()
